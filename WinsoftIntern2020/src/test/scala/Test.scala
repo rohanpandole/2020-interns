f1481855d3f9334792eb57bb2b9bb30c3a6504f3
rohanpandole <rohanpandole@gmail.com>
2020-07-21 23:24:47 +0530

    Added library Dependencies of spark-core and spark-sql

--- WinsoftIntern2020/src/test/scala/Test.scala
+++ WinsoftIntern2020/src/test/scala/Test.scala
@@ -1,3 +1,13 @@
-object Test {
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+object Test
+{
+  def main(args: Array[String]): Unit =
+  {
+    Logger.getLogger("org").setLevel(Level ERROR)
+    val spa=SparkSession.builder.master("local[*]").getOrCreate()
+
+
+  }
 
 }
