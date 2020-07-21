cfe3f353d6cbe14dc1f1f7cc1276cd7cb4080f52
rohanpandole <rohanpandole@gmail.com>
2020-07-21 23:49:46 +0530

    Filtered raw json dataset to usefull data schema

--- WinsoftIntern2020/src/test/scala/Test.scala
+++ WinsoftIntern2020/src/test/scala/Test.scala
@@ -7,6 +7,22 @@ object Test
     Logger.getLogger("org").setLevel(Level ERROR)
     val spa=SparkSession.builder.master("local[*]").getOrCreate()
 
+    import spa.implicits._
+    val dataFrame = spa.read.json("/home/rohan/Spark_course_materials/SparkScala3/JsonData/demoData.json").toDF()
+
+    dataFrame.show(false)
+    dataFrame.printSchema()
+    val nestedDays = dataFrame.selectExpr("rates.*")
+
+    val dayColumn = nestedDays.columns.map(c => s"'$c', `$c`").mkString(", ")
+
+    val filteredData = nestedDays.selectExpr(s"stack(${nestedDays.columns.length}, $dayColumn) as (`yyyy-MM-dd`, value)")
+      .filter(s"`yyyy-MM-dd` between '2019-01-02' and '2019-01-04'")
+      .selectExpr("`yyyy-MM-dd`", "value.*")
+
+    filteredData.printSchema()
+    filteredData.show()
+
 
   }
 
